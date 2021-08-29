583a556878288b06471fdd086d8ae95c4cbe576e
Savioli Pri <stobbeglaucya@gmail.com>
2021-08-29 19:18:07 -0300

    Adicionar resoluçao exercicio cidades

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Praia grande!");
+            Console.WriteLine("Mongaguá!");
+            Console.WriteLine("Itanhaém!");
+            Console.WriteLine("Peruíbe!");
+        }
+    }
+}
